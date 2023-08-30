89a633a0dc74a42d961e75409216a64c68835c55
Jungkook Yoo <yoojeebee@kakao.com>
2023-08-30 19:33:22 +0900

    --- MENU.java 클래스 미완성

--- KOSTAmini1/src/Menu.java
+++ KOSTAmini1/src/Menu.java
@@ -1,17 +1,29 @@
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import common.MENU;
+import common.VO;
 import dao.MeetDao;
+import menu.MeetMenu;
 import service.MeetService;
+import vo.Meet;
 
-public class Menu {
+public class Menu<T extends VO> {
     private Scanner sc;
+    private List<MENU<T>> list;
 
     public Menu() {
         sc = new Scanner(System.in);
+        list = new ArrayList<>(){
+            {
+                add(null); add(null);
+            }
+        };
     }
 
     public void menu() {
-        
         while(true) {
             System.out.println("-----------------------------------------------------");
             System.out.println("1. 모집 / 2. 종료");
@@ -21,7 +33,7 @@ public class Menu {
 
             switch(num) {
                 case 1:
-                    meet();
+                    objCheck(num);
                     break;
                 case 2:
                     System.exit(0);
@@ -30,33 +42,8 @@ public class Menu {
         }
     }
 
-    public void meet() {
-        MeetService meetService = new MeetService(sc, new MeetDao<>());
-        boolean flag = true;
-        while(flag) {
-            System.out.println("-----------------------------------------------------");
-            meetService.menu(-1);
-            System.out.println("1. 모집 게시글 상세조회 / 2. 모집 게시글 작성 / 5. 종료");
-            System.out.println("-----------------------------------------------------");
-            System.out.print(": ");
-            int num = sc.nextInt();
-
-            switch(num) {
-                case 1:
-                    infoMeet();
-                    break;
-                case 2:
-                    meetService.menu(num);
-                    break;
-                case 10:
-                    flag = false;
-                    break;
-            }
-        }
-    }
-
-    public void infoMeet() {
-        System.out.println("1. 참가 / 2. 참가 취소 / 3. 댓글 작성");
-        System.out.println("-----------------------------------------------------");
+    public void objCheck(int num) {
+        if(num == 1 && list.get(0) == null) list.add(1, (MENU<T>)new MeetMenu(new MeetService(sc, new MeetDao<Meet>()), sc));
+        else list.get(num).menu();
     }
 }
